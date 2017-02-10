d882bc0f91cded7b6489bf92d756d4f67c62eafc
203p21 <203p21@203p08-PC>
2017-02-10 09:37:52 +0100

    Primer puno

---
+++ src/fechaa/Fechaa.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fechaa;
+
+/**
+ *
+ * @author 203p21
+ */
+public class Fechaa {
+
+    int dia;
+    int mes;
+    int ano;
+    
+}
